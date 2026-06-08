e08f32669b1a096e3a2aae9147533224139145ee
9prabin77-lang <9prabin77@gmail.com>
2026-06-08 09:19:26 +0545

    async

---
+++ asyncnawait.js
@@ -0,0 +1,15 @@
+let fetchAllUsers = async()=>{
+    // https://jsonplaceholder.typicode.com/users
+    try{
+        let response =await fetch("https://jsonplaceholder.typicode.com/users")
+        let data =await response.json()
+        console.log("all user data: ",data)
+    }
+    catch(e){
+        console.log("error in fetching users")
+    }
+    finally{
+    console.log("After fetching all users")
+    }
+}
+fetchAllUsers()
