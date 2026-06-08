08d1681a3732de569cce65e0ca4b3a725b84452c
9prabin77-lang <9prabin77@gmail.com>
2026-06-08 08:37:43 +0545

    promises

--- promises.js
+++ promises.js
@@ -1,1 +1,31 @@
 console.log("as")
+let promises = new Promise(
+    //callback function
+    (resolve,reject)=>{
+        let tmrolagitarajhardinxu = false;
+        if(tmrolagitarajhardinxu){
+            resolve("hera maile tara jhardiye")
+
+        }else{
+            reject("maile jharna sakina")
+        }
+    }
+)
+
+let kasamkhau = ()=>{
+    promises.then(
+        (data)=>{
+            console.log("success: ",data)
+        }
+    ).catch(
+        (e)=>{
+            console.log("Error: ",e)
+        }
+    ).finally(
+        ()=>{
+            console.log("k garney runey ki hasney")
+        }
+    )
+}
+
+kasamkhau()
