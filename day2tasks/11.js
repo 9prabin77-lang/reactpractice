57f30e8097a5a08f59c88517392fefdb0d45c47c
9prabin77-lang <9prabin77@gmail.com>
2026-06-09 09:05:34 +0545

    pro

--- day2tasks/11.js
+++ day2tasks/11.js
@@ -1,8 +1,31 @@
 const user = {
   name: "Prabin",
   greet() {
-    console.log(`Hello, my name is ${this.name}`);
+    console.log(`Hello, my name is ${this.name}`)
   }
-};
+}
+user.greet()
 
-user.greet();
+
+const secondGreet = user.greet.bind(user)
+secondGreet()
+
+const guy1 = { name: "Do" }
+const guy2 = { name: "Re" }
+
+function call() {
+  console.log(`calling ${this.name}`)
+}
+
+
+const callDo = call.bind(guy1)
+const callRe = call.bind(guy2)
+
+callDo()
+callRe()
+
+
+
+
+
+
