9c93b280b3b92f6b7db717fa41d1cdc81084f6de
9prabin77-lang <9prabin77@gmail.com>
2026-06-29 09:39:11 +0545

    Update dynamictodos.jsx

--- day7/dynamictodos.jsx
+++ day7/dynamictodos.jsx
@@ -8,10 +8,23 @@ const DyListTodos =()=>{
                 title: "Beautify dynamic List Todo",
                 desc:"complete the CSS",
                 date:"06-30-2026",
-                compltete:"false",
+                complete:false,
             }
         ]
     )
+
+
+    let [title,setTitle]=useState("")
+    let [desc,setDesc]=useState("")
+    let [date,setDate]=useState(new Date().toISOString().split('T')(0))
+    let [complete,setComplete]=useState("")
+
+    let handleTitle=(e)=>setTitle(e.target.value)
+    let handleDesc=(e)=>setDesc(e.target.value)
+    let handleDate=(e)=>setDate(e.target.value)
+    let handleComplete=(e)=>setComplete(e.target.checked)
+
+    
 
     return(
         
@@ -19,16 +32,16 @@ const DyListTodos =()=>{
             <h1><b>ToDos Form</b></h1>
             <div className="todo-form">
                 <label htmlFor="todo-title">Title:
-                <input id="todo-title"  type="text" placeholder="enter title"/>
+                <input value={title} onChange={handleTitle} id="todo-title"  type="text" placeholder="enter title"/>
                 </label><br/>
                 <label htmlFor="todo-desc">Description:
-                <textarea id="todo-desc" type="textarea" placeholder="enter the description" />
+                <textarea value={desc} onChange={handleDesc} id="todo-desc" type="textarea" placeholder="enter the description" />
                 </label><br/>
                 <label htmlFor="todo-date">Date:
-                <input id="todo-date" type="date"/>
+                <input value={date} onChange={handleDate} id="todo-date" type="date"/>
                 </label><br/>
-                <label>
-                    <input type="checkbox"/>Completed
+                <label htmlFor="todo-status">
+                    <input value={complete} onChange={handleComplete} type="checkbox" id="todo-status"/>Completed
                 </label><br/>
                 <button>Add ToDo</button>
             </div>
@@ -40,7 +53,7 @@ const DyListTodos =()=>{
                         <h4>{el.title}</h4>
                         <p>{el.desc}</p>
                         <b>Date; {el.date}</b>
-                        <b>Completed:{el.compltete? "true" : "false"}</b>
+                        <b>Completed:{el.complete? "true" : "false"}</b>
                     </div>
                 )
             })
